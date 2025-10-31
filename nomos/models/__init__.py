f5361731bf425d0a1e450df9f628140b21e50874
chandralegend <irugalbandarachandra@gmail.com>
2025-10-31 23:07:45 +0530

    new beginning - be ready for new journey.

--- nomos/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Models for Nomos."""
-
-from .agent import *  # noqa
-from .flow import *  # noqa
-from .tool import *  # noqa
--- support/ts-sdk/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  env: { node: true, es2020: true },
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['dist'],
-  plugins: ['@typescript-eslint'],
-  rules: {},
-};
